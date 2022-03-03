39253d88d35f09e38fe9bba8ee785ec137d2e4c1
Sirasit Boonklang <46419278+aeff60@users.noreply.github.com>
2022-03-03 13:15:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,17 @@ app  = Flask(__name__)
 @app.route('/')
 
 def home():
-    return render_template('home.html')
+    api_key = 'VN4aWcnxQzBqRhcMJ7SwKWavIs1goZDcLQ8oSx9s2WnA7ztpYgqosJkymEc1xogF'
+    api_secret = 'fNYc0CWm9CYPhBo7rTcX5RfV7MNsMIivcrdYHYycdQCFmrQ0U67vwgKHnuW2zcNO'
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+    client = Client(api_key, api_secret)
+
+    info = client.get_account()
+
+
+    for asset in info['balances']:
+        data = asset['asset']
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)    
