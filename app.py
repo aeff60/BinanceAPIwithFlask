7d1f3eb6779087bd4eaefc0433d49f5872e31704
Sirasit Boonklang <46419278+aeff60@users.noreply.github.com>
2022-03-03 16:11:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,44 @@
 from flask import Flask
 from flask import Flask, render_template
+from binance.client import Client
+import json
+from pprint import pprint
 
 app  = Flask(__name__)
 
 @app.route('/')
 
 def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    
     api_key = 'VN4aWcnxQzBqRhcMJ7SwKWavIs1goZDcLQ8oSx9s2WnA7ztpYgqosJkymEc1xogF'
     api_secret = 'fNYc0CWm9CYPhBo7rTcX5RfV7MNsMIivcrdYHYycdQCFmrQ0U67vwgKHnuW2zcNO'
 
     client = Client(api_key, api_secret)
 
     info = client.get_account()
+    # pprint(info)
 
+    # doge = client.get_asset_balance(asset='DOGE')
+    # usdt = client.get_asset_balance(asset='USDT')
 
+    # print('DOGE:', doge)
+    # print('USDT:', usdt)
+    
+    print(info['balances'])
+    
+    tempHTML = ""
+    
     for asset in info['balances']:
-        data = asset['asset']
-    return render_template('home.html')
+        
+        tempHTML.join("<li>",asset,"</li>")
+        
+        print(asset['asset'])
+
+    return tempHTML
 
 if __name__ == '__main__':
     app.run(debug=True)    
