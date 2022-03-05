9d094c3f1361ec965a47bd2e037f3644b7373265
Sirasit0397 <ssbunklnag@gmail.com>
2022-03-05 12:50:10 +0700

    before try cookie

--- app.py
+++ app.py
@@ -1,44 +1,43 @@
+from asyncio.windows_events import NULL
 from flask import Flask
 from flask import Flask, render_template
-from binance.client import Client
 import json
-from pprint import pprint
+from urllib.request import urlopen
 
 app  = Flask(__name__)
 
 @app.route('/')
 
 def home():
-    return render_template('home.html')
+    
+    url = "https://api.binance.com/api/v1/ticker/price"
+
+    response = urlopen(url)
+
+    data_json = json.loads(response.read())
+    response = urlopen(url)
+  
+
+    data_json = json.loads(response.read())
+    symbolList = []
+    a = 1
+    for i in range(len(data_json)):
+        symbol = data_json[i].get('symbol')
+        symbolList.append(symbol)
+    return render_template('home.html', data=data_json)
 
 @app.route('/about')
 def about():
+    url = "https://api.binance.com/api/v1/ticker/price"
+    response = urlopen(url)
+    data_json = json.loads(response.read())
+    response = urlopen(url)
+    data_json = json.loads(response.read())
+    symbolList = []
+    for i in range(len(data_json)):
+        symbol = data_json[i].get('symbol')
+        symbolList.append(symbol)
+    return data_json
     
-    api_key = 'VN4aWcnxQzBqRhcMJ7SwKWavIs1goZDcLQ8oSx9s2WnA7ztpYgqosJkymEc1xogF'
-    api_secret = 'fNYc0CWm9CYPhBo7rTcX5RfV7MNsMIivcrdYHYycdQCFmrQ0U67vwgKHnuW2zcNO'
-
-    client = Client(api_key, api_secret)
-
-    info = client.get_account()
-    # pprint(info)
-
-    # doge = client.get_asset_balance(asset='DOGE')
-    # usdt = client.get_asset_balance(asset='USDT')
-
-    # print('DOGE:', doge)
-    # print('USDT:', usdt)
-    
-    print(info['balances'])
-    
-    tempHTML = ""
-    
-    for asset in info['balances']:
-        
-        tempHTML.join("<li>",asset,"</li>")
-        
-        print(asset['asset'])
-
-    return tempHTML
-
 if __name__ == '__main__':
     app.run(debug=True)    
