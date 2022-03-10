09b716b32f1a4a0c5a5770260a0b7aa7c1d5e875
Aef_SB <46419278+aeff60@users.noreply.github.com>
2022-03-11 01:30:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-
+import os
+from app import app
 from flask import *  
 import json
 from urllib.request import urlopen
@@ -73,8 +74,10 @@ def home():
     return resp
       
     
-if __name__ == '__main__':
-    app.run(debug=True)    
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+   
 
 
 
